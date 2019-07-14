e9c71ae9f00491740a0c854f886d202cfefd5cdf
Pat Lillis <lillispm@gmail.com>
2019-07-13 21:25:27 -0600

    "Styling Components" lesson

--- pages/index.js
+++ pages/index.js
@@ -1,33 +1,60 @@
 import Layout from '../components/MyLayout';
 import Link from 'next/link';
-import fetch from 'isomorphic-unfetch';
 
-function Index(props) {
+function getPosts() {
+  return [
+    { id: 'hello-nextjs', title: 'Hello Next.js' },
+    { id: 'learn-nextjs', title: 'Learn Next.js is awesome' },
+    { id: 'deploy-nextjs', title: 'Deploy apps with ZEIT' },
+  ];
+}
+
+function PostLink({ post }) {
+  return (
+    <li>
+      <Link href="/p/[id]" as={`/p/${post.id}`}>
+        <a>{post.title}</a>
+      </Link>
+      <style jsx>{`
+        li {
+          list-style: none;
+          margin: 5px 0;
+        }
+
+        a {
+          text-decoration: none;
+          color: blue;
+          font-family: 'Arial';
+        }
+
+        a:hover {
+          opacity: 0.6;
+        }
+      `}</style>
+    </li>
+  );
+}
+
+function Blog() {
   return (
     <Layout>
-      <h1>Batman TV Shows</h1>
+      <h1>My Blog</h1>
       <ul>
-        {props.shows.map(show => (
-          <li key={show.id}>
-            <Link href="/p/[id]" as={`/p/${show.id}`}>
-              <a>{show.name}</a>
-            </Link>
-          </li>
+        {getPosts().map(post => (
+          <PostLink key={post.id} post={post} />
         ))}
       </ul>
+      <style jsx>{`
+        h1 {
+          font-family: 'Arial';
+        }
+
+        ul {
+          padding: 0;
+        }
+      `}</style>
     </Layout>
   );
 }
 
-Index.getInitialProps = async function() {
-  const result = await fetch('https://api.tvmaze.com/search/shows?q=batman');
-  const data = await result.json();
-
-  console.log(`Show data fetched. Count: ${data.length}`);
-
-  return {
-    shows: data.map(entry => entry.show),
-  };
-};
-
-export default Index;
+export default Blog;
